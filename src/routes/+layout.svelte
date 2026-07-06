3164b101a9b1f3be3c4eeab57ac9251fa12b6a39
pr0g3t <killianott4@gmail.com>
2026-07-06 20:29:45 +0200

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import SiteFooter from '$lib/components/SiteFooter.svelte';
 	import SiteHeader from '$lib/components/SiteHeader.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <a class="skip-link" href="#main">Aller au contenu</a>
 
